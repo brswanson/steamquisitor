30f6b8e7dce16132271e649348ab8eb45acf9af9
Ben Swanson <swansob@gmail.com>
2018-03-18 15:56:09 -0400

    Moved Game and Friend lists out into separate components. Game and Friend data now displayed usings props of the child components from the parent App component instead of App state. Child components only displayed if data exists for the queried UserID. Also limited the number of rows to display for each type to 10.

--- steamquisitor/src/App.js
+++ steamquisitor/src/App.js
@@ -14,9 +14,12 @@ class App extends Component {
     this.state = {
       // Defaulting to the SteamID of the author of the app
       id: 76561197972291669,
-      response: '',
+
+      gamesLoaded: false,
       gameCount: 0,
       games: [],
+
+      friendsLoaded: false,
       friends: [],
     };
 
@@ -35,21 +38,29 @@ class App extends Component {
   }
 
   displayGames() {
+    this.setState({ gamesLoading: true });
+
     this.apiCall(ApiOwnedGames)
       .then(res => this.setState({
         response: res.response,
         gameCount: res.response.game_count,
         // Sort games desc by play time, then asc id
-        games: _.orderBy(res.response.games, ['playtime_forever', 'appid'], ['desc', 'asc'])
+        // Only takes the first 10 elements
+        games: _.take(_.orderBy(res.response.games, ['playtime_forever', 'appid'], ['desc', 'asc']), 10),
+        gamesLoaded: true,
       }))
       .catch(err => console.log(err));
   }
 
   displayFriendList() {
+    this.setState({ friendsLoading: true });
+
     this.apiCall(ApiFriendsList)
       .then(res => this.setState({
         // Sort friends by time since friend was added desc
-        friends: _.orderBy(res.friendslist.friends, ['friend_since'], ['desc'])
+        // Only takes the first 10 elements
+        friends: _.take(_.orderBy(res.friendslist.friends, ['friend_since'], ['desc']), 10),
+        friendsLoaded: true,
       }))
       .catch(err => console.log(err));
   }
@@ -65,34 +76,6 @@ class App extends Component {
 
   changeId(e) {
     this.setState({ id: e.target.value });
-  }
-
-  // TODO: Should move this to a helper class
-  convertTimestamp(timestamp) {
-    var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
-      yyyy = d.getFullYear(),
-      mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
-      dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
-      hh = d.getHours(),
-      h = hh,
-      min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
-      ampm = 'AM',
-      time;
-
-    if (hh > 12) {
-      h = hh - 12;
-      ampm = 'PM';
-    } else if (hh === 12) {
-      h = 12;
-      ampm = 'PM';
-    } else if (hh == 0) {
-      h = 12;
-    }
-
-    // ie: 2013-02-18, 8:35 AM	
-    time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
-
-    return time;
   }
 
   render() {
@@ -118,22 +101,31 @@ class App extends Component {
           <button onClick={this.refreshUser}>Refresh</button>
         </div>
 
-        <p className="App-intro">Game Count: {this.state.gameCount}</p>
+        {this.state.gamesLoaded && <GamesList gameCount={this.state.gameCount} games={this.state.games} />}
+
+        {this.state.friendsLoaded && <FriendsList friends={this.state.friends} />}
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+class GamesList extends Component {
+  // <props>
+  // gameCount: 0
+  // games: []
+
+  render() {
+    return (
+      <div>
+        <p className="App-intro">Game Count: {this.props.gameCount}</p>
         <ul>
-          {this.state.games.map((item, index) =>
+          {this.props.games.map((item, index) =>
             <li key={index}>
               <a href={'http://store.steampowered.com/app/' + item.appid}>{item.appid} Playtime: {item.playtime_forever} minutes
               {item.playtime_2weeks ? `, Last 2 Weeks: ${item.playtime_2weeks}` : ''}
-              </a>
-            </li>
-          )}
-        </ul>
-        <p className="App-intro">Friends List</p>
-        <ul>
-          {this.state.friends.map((item, index) =>
-            <li key={index}>
-              <a href={`http://steamcommunity.com/profiles/` + item.steamid}>{item.steamid}
-              {item.friend_since > 0 ? ` Friend Since: ${this.convertTimestamp(item.friend_since)}` : ''}
               </a>
             </li>
           )}
@@ -143,4 +135,52 @@ class App extends Component {
   }
 }
 
-export default App;
+class FriendsList extends Component {
+  // <props>
+  // friends: []
+
+  render() {
+    return (
+      <div>
+        <p className="App-intro">Friends List</p>
+        <ul>
+          {this.props.friends.map((item, index) =>
+            <li key={index}>
+              <a href={`http://steamcommunity.com/profiles/` + item.steamid}>{item.steamid}
+                {item.friend_since > 0 ? ` Friend Since: ${this.convertTimestamp(item.friend_since)}` : ''}
+              </a>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+
+  // TODO: Should move this to a helper class
+  convertTimestamp(timestamp) {
+    var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+      yyyy = d.getFullYear(),
+      mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+      dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+      hh = d.getHours(),
+      h = hh,
+      min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+      ampm = 'AM',
+      time;
+
+    if (hh > 12) {
+      h = hh - 12;
+      ampm = 'PM';
+    } else if (hh === 12) {
+      h = 12;
+      ampm = 'PM';
+    } else if (hh === 0) {
+      h = 12;
+    }
+
+    // ie: 2013-02-18, 8:35 AM	
+    time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+
+    return time;
+  }
+}
